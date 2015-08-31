f7af0a869ea1a96b6a87c31e715c2b5aaece58e1
Sanver <sanvertarmur@gmail.com>
2015-08-31 23:17:24 +0300

    predict

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -21,9 +21,19 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+%Add a row that contains ones at top of matrix
+X = [ones(m, 1) X];
 
+Z2 = X * Theta1';
+A2 = sigmoid(Z2);
 
+A2 = [ones(size(A2), 1) A2];
 
+Z3 = A2 * Theta2';
+A3 = sigmoid(Z3);
+
+% Get max row value and max row idx of all rows in a matrix.
+[maxValue p] = max (A3, [], 2);
 
 
 
